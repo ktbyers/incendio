e7519246cf9018a61ffe35fcbf91f5558443f8ef
Kirk Byers <ktbyers@twb-tech.com>
2019-09-24 17:31:12 -0700

    Import all of the standard platform classes in the test

--- test/test_import_incendio.py
+++ test/test_import_incendio.py
@@ -1,4 +1,10 @@
 import incendio  # noqa
+from incendio.eos.eos import EOSDriver  # noqa
+from incendio.ios.ios import IOSDriver  # noqa
+from incendio.nxos.nxos import NXOSDriver  # noqa
+from incendio.nxos_ssh.nxos_ssh import NXOSSSHDriver  # noqa
+from incendio.junos.junos import JunOSDriver  # noqa
+from incendio.iosxr.iosxr import IOSXRDriver  # noqa
 
 
 def test_placeholder():
